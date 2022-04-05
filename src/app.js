d279311940e6279c4b62db2c614228e230a08b02
barbosa92 <alexrp_92@hotmail.com>
2022-04-05 15:47:05 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adjetive = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ending = [".es", ".net", ".org"];
+
+  let firstElement = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let secondElement = adjetive[Math.floor(Math.random() * adjetive.length)];
+  let thirdElement = noun[Math.floor(Math.random() * noun.length)];
+  let fourthElement = ending[Math.floor(Math.random() * ending.length)];
+
+  let dominio = `${firstElement}${secondElement}${thirdElement}${fourthElement}`;
+  document.querySelector("#domain").innerHTML = dominio;
 };
