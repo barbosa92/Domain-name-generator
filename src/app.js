89ccdcf28a541500dbd2ce33f4a42d65f6f5d471
barbosa92 <alexrp_92@hotmail.com>
2022-04-15 17:42:05 +0000

    Proyecto corregido y finalizado

--- src/app.js
+++ src/app.js
@@ -8,11 +8,16 @@ window.onload = function() {
   let noun = ["jogger", "racoon"];
   let ending = [".es", ".net", ".org"];
 
-  let firstElement = pronoun[Math.floor(Math.random() * pronoun.length)];
-  let secondElement = adjetive[Math.floor(Math.random() * adjetive.length)];
-  let thirdElement = noun[Math.floor(Math.random() * noun.length)];
-  let fourthElement = ending[Math.floor(Math.random() * ending.length)];
-
-  let dominio = `${firstElement}${secondElement}${thirdElement}${fourthElement}`;
-  document.querySelector("#domain").innerHTML = dominio;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjetive.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ending.length; l++) {
+          let dominio = pronoun[i] + adjetive[j] + noun[k] + ending[l];
+          let h6 = document.createElement("h6");
+          h6.innerHTML = dominio;
+          document.body.appendChild(h6);
+        }
+      }
+    }
+  }
 };
